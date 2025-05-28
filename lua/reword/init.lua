4869d53e67d07d9cad6770ec22ac9256379adfdf
Martin Cornelius <martincornelius.nielsen@gmail.com>
2025-05-28 16:43:05 +0200

    feat: simplest reword usage

---
+++ lua/reword/init.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+local api = require("reword.api")
+local ui = require("reword.ui")
+
+function M.reword_word()
+  local word = vim.fn.expand("<cword>")
+  if not word or word == "" then
+    vim.notify("No word found under cursor", vim.log.levels.WARN)
+    return
+  end
+
+  api.get_synonyms(word, function(synonyms)
+    if not synonyms or #synonyms == 0 then
+      vim.notify("No synonyms found", vim.log.levels.INFO)
+      return
+    end
+    ui.select_synonym(word, synonyms)
+  end)
+end
+
+return M
